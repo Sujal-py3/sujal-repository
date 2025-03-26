ddf1e02a6a6c0450cbc4b94cbdd8bd90b998e7c5
Sujal Singh <sujalsingh31772@gmail.com>
2025-03-26 12:45:35 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/your_twitter_handle",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@your_twitter_handle",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:sujal3177@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "sujal3177@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Sujal-py3",
 		label: "Github",
-		handle: "chronark",
+		handle: "Sujal-py3",
 	},
 ];
 
